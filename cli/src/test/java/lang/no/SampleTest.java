3c162ec5081e7592b8c3b220a7d788275e8a94b5
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-06 22:43:58 +0800

    ???

--- cli/src/test/java/lang/no/SampleTest.java
+++ cli/src/test/java/lang/no/SampleTest.java
@@ -2,6 +2,7 @@ package lang.no;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import lang.no.parser;
 
 public class SampleTest {
     @Test void testOne() {
