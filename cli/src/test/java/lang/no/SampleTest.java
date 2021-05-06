bc4455de631f056837c59fb66b77894a8e6f7445
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-06 22:47:32 +0800

    aha! so basically, dependency for test should be added

--- cli/src/test/java/lang/no/SampleTest.java
+++ cli/src/test/java/lang/no/SampleTest.java
@@ -2,7 +2,7 @@ package lang.no;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import lang.no.parser;
+import lang.no.parser.NoParser;
 
 public class SampleTest {
     @Test void testOne() {
