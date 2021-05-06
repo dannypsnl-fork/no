315a91a7d0b593503a95da540c2f99b3e251e7a5
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-06 22:34:58 +0800

    test

---
+++ cli/src/test/java/lang/no/SampleTest.java
@@ -0,0 +1,11 @@
+package lang.no;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+    @Test void testOne() {
+        assertEquals(1, 1);
+    }
+}
