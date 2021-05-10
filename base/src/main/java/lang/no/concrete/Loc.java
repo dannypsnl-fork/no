85949b4dfd55ac84d19c2346365b28cec923eda1
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-10 15:54:46 +0800

    [base] give default value and access for Location values

--- base/src/main/java/lang/no/concrete/Loc.java
+++ base/src/main/java/lang/no/concrete/Loc.java
@@ -3,7 +3,7 @@ package lang.no.concrete;
 import org.antlr.v4.runtime.Token;
 
 public class Loc {
-    int line, column, endLine, endColumn;
+    private int line = 1, column = 0, endLine = 1, endColumn = 0;
 
     public Loc(Token token, Token endToken) {
         line = token.getLine();
@@ -11,4 +11,9 @@ public class Loc {
         endLine = endToken.getLine();
         endColumn = endToken.getCharPositionInLine();
     }
+
+    public int getLine() { return line; }
+    public int getColumn() { return column; }
+    public int getEndLine() { return endLine; }
+    public int getEndColumn() { return endColumn; }
 }
